95560c3e8a2173e65d1f56c827cf42787ad03a96
Robert Haase <haesleinhuepf@users.noreply.github.com>
2023-04-20 15:19:36 +0200

    More complex Jython example

--- demo/basic_demo.py
+++ demo/basic_demo.py
@@ -1,5 +1,19 @@
 # This Jython script can be run from within FIJI's script editor
-
+from ij import IJ
 from net.clesperanto import CLIJ3
 
 cle = CLIJ3.getInstance()
+
+imp = IJ.openImage("http://imagej.nih.gov/ij/images/blobs.gif");
+imp.show()
+
+gpu_image = cle.push(imp)
+
+gpu_result = cle.add_image_and_scalar(gpu_image, None, 1000)
+
+imp_res = cle.pull(gpu_result)
+imp_res.show()
+
+
+
+
