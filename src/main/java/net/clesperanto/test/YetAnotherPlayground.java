5fb98096f25a3e5c53a1a7f6d7baabef2aff6f0a
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2024-08-26 14:11:23 +0200

    update tests

--- src/main/java/net/clesperanto/test/YetAnotherPlayground.java
+++ src/main/java/net/clesperanto/test/YetAnotherPlayground.java
@@ -1,7 +1,5 @@
 package net.clesperanto.test;
 
-import java.lang.reflect.Array;
-
 import ij.IJ;
 import ij.ImageJ;
 import ij.ImagePlus;
@@ -15,15 +13,13 @@ public class YetAnotherPlayground {
         new ImageJ();
 
         ImagePlus imp = IJ.openImage("./demo/blobs.tif");
-        // IJ.run(imp, "32-bit", "");
         imp.show();
 
         CLIJ3 cle = CLIJ3.getInstance();
         
-        ArrayJ labels = cle.create_like(imp);
         ArrayJ blurred = cle.gaussian_blur(imp, null, 10, 0, 0);
         ArrayJ binary = cle.threshold_otsu(blurred, null);
-        cle.connected_components_labeling(binary, labels);
+        ArrayJ labels = cle.connected_components_labeling(binary, null);
 
 
         cle.imshow(labels);
